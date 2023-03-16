79f4bdfcf12185f0af6dc46cdbe362dc0fc4c5ca
Thanachok Rattanamora <schweppeweb@gmail.com>
2023-03-16 10:01:09 +0700

    Initial Commit

---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by Schweppe on 16/3/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
